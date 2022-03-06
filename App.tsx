164995c2063b33c0dfa3dad2281206296acd432b
DanFeLoGar <danypolo18@hotmail.com>
2022-03-05 22:22:43 -0500

    first commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+//1) Set up redux
+//2)
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Test RN with build at uber clone</Text>
       <StatusBar style="auto" />
     </View>
   );
